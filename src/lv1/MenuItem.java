abc51e4f8fd0be2e7dc0a509995a1d6f45f14e1e
leetwin1115 <jjeepp1115@gmail.com>
2025-07-16 16:18:24 +0900

    lv1 완료

---
+++ src/lv1/MenuItem.java
@@ -0,0 +1,16 @@
+package lv1;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class MenuItem {
+    //속
+    private final List<String> menu= new ArrayList<>();
+
+    //생
+
+    //기
+
+
+
+}
